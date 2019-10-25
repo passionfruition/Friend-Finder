e1eac9cf8ce6ed3905f1fbfc00d81f518a615a4f
Madeleine Prak <madeleineprak@gmail.com>
2019-10-24 23:16:07 -0700

    Added more existing buddies

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,5 +3,15 @@ module.exports = [
         name: "Kanye West",
         photo: "https://i1.wp.com/metro.co.uk/wp-content/uploads/2018/08/gettyimages-1014299150-e1536307443119.jpg?quality=90&strip=all&zoom=1&resize=644%2C416&ssl=1",
         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Post Malone",
+        photo: "https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/tkxndk4gl61ruxtjqtyb/post-malone-siriusxm",
+        scores: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    },
+    {
+        name: "Cardi B",
+        photo: "https://img.resized.co/spin1038/eyJkYXRhIjoie1widXJsXCI6XCJodHRwczpcXFwvXFxcL3MzLWV1LXdlc3QtMS5hbWF6b25hd3MuY29tXFxcL3N0b3JhZ2UucHVibGlzaGVycGx1cy5pZVxcXC9tZWRpYS5yYWRpb2Ntcy5uZXRcXFwvdXBsb2Fkc1xcXC8yMDE5XFxcLzAyXFxcLzE0MTAzMTQ4XFxcL1NQTDUwNTM4MjlfMDAxLWUxNTUwMTQwMzIzODMyLTEwMjR4NDgyLmpwZ1wiLFwid2lkdGhcIjo5NzAsXCJoZWlnaHRcIjo0ODUsXCJkZWZhdWx0XCI6XCJodHRwczpcXFwvXFxcL3d3dy5zcGluMTAzOC5jb21cXFwvaW1hZ2VzXFxcL25vLWltYWdlLnBuZ1wifSIsImhhc2giOiJiYTA2ZWFlMzBlNWYzOTg3MGNlZGMxYWMxMDllNDZmZjMwNzM0Mzg5In0=/cardi-b-reactivates-instagram-to-reveal-new-collab-with-bruno-mars.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ]
